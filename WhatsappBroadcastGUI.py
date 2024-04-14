d15faa88a5ff4e7372f4ca6bfa88487c84aa6f88
Yao <cyaoen@gmail.com>
2024-04-14 19:35:04 +0800

    remove customization message

--- WhatsappBroadcastGUI.py
+++ WhatsappBroadcastGUI.py
@@ -1,3 +1,5 @@
+import threading
+
 from PIL import Image
 from tkinter import messagebox
 
@@ -5,50 +7,80 @@ import os
 import customtkinter
 import pandas as pd
 
-import WhatsappSendMessage
+from WhatsappSendMessage import WhatsappParentNotice
 
 
 class WhatsappParentNoticeGUI(customtkinter.CTk):
 
+    def button_trigger_event(self):
+        if self.tabview.get() == self.TAB1:
+            if self.btn_send_message.cget('text') == self.STRING_BATCH_SEND:
+                if not self.display_example_event():
+                    return
+
+                self.btn_send_message.configure(text=self.STRING_CANCEL_SEND)
+
+                whatsapp_thread = threading.Thread(target=lambda: (
+                    self.whatsapp.set_value(
+                        self.string_filepath.get(),
+                        float(self.option_delay.get())
+                    ),
+                    self.whatsapp.run(),
+                    self.btn_send_message.configure(text=self.STRING_BATCH_SEND)
+                ))
+                whatsapp_thread.start()
+
+            elif self.btn_send_message.cget("text") == self.STRING_CANCEL_SEND:
+                self.btn_send_message.configure(text=self.STRING_BATCH_SEND)
+                self.whatsapp.set_stop(True)
+
     def display_example_event(self):
-        if not self.file_validate():
-            return
+        if not self.file_existing_check():
+            return False
 
         df = pd.read_excel(self.string_filepath.get())
-        column_to_check = ['寝室', '姓名', '日期', '时间', '监护人电话', '离舍']
 
-        if not all(column in df.columns for column in column_to_check):
-            messagebox.showerror(title="错误", message=f'Excel文件表头须拥有{column_to_check}\n请查阅"使用须知"')
-            return
+        if self.tabview.get() == self.TAB1:
+            column_to_check = self.whatsapp.staying_leaving_columns
 
-        df.set_index('姓名', inplace=True)
-        is_displayed_leave = False
-        is_display_stay = False
+            if not all(column in df.columns for column in column_to_check):
+                messagebox.showerror(title="错误", message=f'Excel文件表头须拥有{column_to_check}\n请查阅"使用须知"')
+                return False
 
-        for name, rows in df.iterrows():
-            if is_display_stay and is_displayed_leave:
-                break
+            df.set_index('姓名', inplace=True)
+            is_displayed_leave = False
+            is_display_stay = False
 
-            if rows['离舍'] == 1 and not is_displayed_leave:
-                text_date = ' (星期' + WhatsappSendMessage.WhatsappParentNotice.get_date_of_week(rows['日期']) + ')'
-                timing = str(rows['日期'])[:10] + text_date + ' ' + str(rows['时间'])[:5]
-                message = f'{name} 同学 {rows['寝室']} 于 {timing} 离校 (回家)。\n敬请家长/监护人关注。'
+            for name, rows in df.iterrows():
 
-                self.string_leaving_sample.set(message)
-                is_displayed_leave = True
+                if rows['离舍'] == 1 and not is_displayed_leave:
+                    text_date = ' (星期' + self.whatsapp.get_date_of_week(rows['日期']) + ')'
+                    timing = str(rows['日期'])[:10] + text_date + ' ' + str(rows['时间'])[:5]
+                    message = f'{name} 同学 {rows['寝室']} 于 {timing} 离校 (回家)。\n敬请家长/监护人关注。'
 
-            elif rows['离舍'] == 0 and not is_display_stay:
-                message = f"{name} 同学 {rows['寝室']} 于 {str(rows['日期'])[:10]} 本周留舍 (留校)。\n敬请家长/监护人关注。"
+                    self.string_leaving_sample.set(message)
+                    is_displayed_leave = True
 
-                self.string_staying_sample.set(message)
+                elif rows['离舍'] == 0 and not is_display_stay:
+                    message = f"{name} 同学 {rows['寝室']} 于 {str(rows['日期'])[:10]} 本周留舍 (留校)。\n敬请家长/监护人关注。"
+
+                    self.string_staying_sample.set(message)
+
+                if is_display_stay and is_displayed_leave:
+                    break
+
+        return True
 
     def filechooser_event(self):
         file_path = customtkinter.filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx")])
-        if self.tabview.get() == self.tab1:
+        if self.tabview.get() == self.TAB1:
             self.string_filepath.set(file_path)
 
-    def file_validate(self):
-        src_file = self.string_filepath.get().strip()
+    # To check whether the file existing
+    def file_existing_check(self):
+        src_file = ""
+        if self.tabview.get() == self.TAB1:
+            src_file = self.string_filepath.get()
 
         if src_file == "":
             messagebox.showerror(title="错误", message="请选择源文件")
@@ -94,13 +126,15 @@ class WhatsappParentNoticeGUI(customtkinter.CTk):
         label_logo.grid(row=5, column=0, padx=5, pady=5)
 
     def tab1_grid_init(self):
-        self.tabview.tab(self.tab1).grid_columnconfigure((0, 1, 2, 3), weight=1)
-        self.tabview.tab(self.tab1).grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7), weight=2)
+        self.tabview.tab(self.TAB1).grid_columnconfigure((0, 1, 2, 3), weight=1)
+        self.tabview.tab(self.TAB1).grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7), weight=2)
 
         # section 1: file chooser
-        label_filepath = customtkinter.CTkLabel(self.tabview.tab(self.tab1), text="文件路径: ")
+        label_filepath = customtkinter.CTkLabel(self.tabview.tab(self.TAB1), text="文件路径: ")
         label_filepath.grid(row=0, column=0, padx=10, pady=10)
-        self.entry_filepath.grid(row=0, column=1, columnspan=2, padx=5, pady=10, sticky="ew")
+        entry_filepath = customtkinter.CTkEntry(self.tabview.tab(self.TAB1), textvariable=self.string_filepath,
+                                                state="disable")
+        entry_filepath.grid(row=0, column=1, columnspan=2, padx=5, pady=10, sticky="ew")
         self.btn_filechooser.grid(row=0, column=3, padx=10, pady=10, sticky="ew")
 
         # section 2: clear or generate sample message
@@ -108,31 +142,38 @@ class WhatsappParentNoticeGUI(customtkinter.CTk):
         self.btn_generate_example.grid(row=1, column=3, padx=10, pady=(20, 10), sticky="ew")
 
         # section 3: To display leaving message
-        label_leave_hostel_eg = customtkinter.CTkLabel(master=self.tabview.tab(self.tab1), text="离舍范本：")
+        label_leave_hostel_eg = customtkinter.CTkLabel(master=self.tabview.tab(self.TAB1), text="离舍范本：")
         label_leave_hostel_eg.grid(row=2, column=0, padx=10, pady=10)
-        self.entry_leaving_sample.grid(row=2, column=1, columnspan=3, padx=10, pady=10, sticky="ew")
+        entry_leaving_sample = customtkinter.CTkEntry(master=self.tabview.tab(self.TAB1),
+                                                      textvariable=self.string_leaving_sample,
+                                                      state="disable")
+        entry_leaving_sample.grid(row=2, column=1, columnspan=3, padx=10, pady=10, sticky="ew")
 
         # section 4: To display staying message
-        label_staying_hostel_eg = customtkinter.CTkLabel(master=self.tabview.tab(self.tab1), text="留舍范本：")
+        label_staying_hostel_eg = customtkinter.CTkLabel(master=self.tabview.tab(self.TAB1), text="留舍范本：")
         label_staying_hostel_eg.grid(row=3, column=0, padx=10, pady=10)
-        self.entry_staying_sample.grid(row=3, column=1, columnspan=3, padx=10, pady=10, sticky="ew")
+        entry_staying_sample = customtkinter.CTkEntry(master=self.tabview.tab(self.TAB1),
+                                                      textvariable=self.string_staying_sample,
+                                                      state="disable")
+        entry_staying_sample.grid(row=3, column=1, columnspan=3, padx=10, pady=10, sticky="ew")
 
         # section 5: Allow to modify the interval of sending message
-        label_delay = customtkinter.CTkLabel(master=self.tabview.tab(self.tab1), text='信息发送延迟间隔：')
+        label_delay = customtkinter.CTkLabel(master=self.tabview.tab(self.TAB1), text='信息发送延迟间隔：')
         label_delay.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
         self.option_delay.grid(row=4, column=2, padx=10, pady=10, sticky="ew")
 
         # section 6: To send message in batch
         self.btn_send_message.grid(row=5, column=3, padx=10, pady=10, sticky="ew")
 
-    def tab2_grid_init(self):
-        self.tabview.tab(self.tab2).grid_columnconfigure((0, 1, 2, 3), weight=1)
-        self.tabview.tab(self.tab2).grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7), weight=2)
+    def tab3_grid_init(self):
+        self.tabview.tab(self.TAB3).grid_columnconfigure((0, 1, 2, 3), weight=1)
+        self.tabview.tab(self.TAB3).grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7), weight=2)
         self.scrollable_frame.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
         # section 1: labelling
-        label_title1 = customtkinter.CTkLabel(master=self.scrollable_frame, text='Excel文件格式样本: ')
-        label_title1.grid(row=0, column=0, padx=1, pady=1, sticky="ew")
+        label_title1 = customtkinter.CTkLabel(master=self.scrollable_frame, text='Excel文件格式样本: ',
+                                              anchor="nw")
+        label_title1.grid(row=0, column=0, columnspan=3, padx=1, pady=1, sticky="ew")
 
         # section 2: data sample display
         data = [('寝室', '姓名', '日期', '时间', '监护人电话', '离舍'),
@@ -143,7 +184,6 @@ class WhatsappParentNoticeGUI(customtkinter.CTk):
 
         for i in range(data_rows_count):
             for j in range(data_columns_count):
-                print(data[i][j])
                 d = customtkinter.CTkLabel(master=self.scrollable_frame,
                                            corner_radius=10,
                                            text_color='black',
@@ -158,50 +198,20 @@ class WhatsappParentNoticeGUI(customtkinter.CTk):
                                                       "2. 时间格式为24小时制 HH:mm:ss (例 18:30:00)\n"
                                                       "3. 监护人电话-马来西亚号码以外都需要加上区号"
                                                       " (例，新加坡 +65)\n"
-                                                      "4. 离舍使用数字 1(离舍) \\ 0(留舍) 区别",
+                                                      "4. 离舍使用数字 '1'(离舍) \\ '0'(留舍) 区别",
                                                  anchor="nw",
                                                  justify="left")
         label_attention.grid(row=3, column=0, padx=5, columnspan=3, pady=(20, 10), sticky="nw")
 
-        #
-        data_format = [('表头栏', '单元格格式', '备注'),
-                       ('寝室', 'General', '楼层+床号'),
-                       ('姓名', 'General', '学生姓名'),
-                       ('日期', 'Date', 'YYYY-MM-DD'),
-                       ('时间', 'Time', '24小时制 18:00:00'),
-                       ('监护人电话', 'Text', '马来西亚以外号码须加上区号 (新加坡 +65)'),
-                       ('离舍', 'General', '"1"为离舍；"0"为留舍')]
-
-        data_format_rows_count = len(data_format)
-        data_format_columns_count = len(data_format[0])
-
-        for i in range(data_format_rows_count):
-            for j in range(data_format_columns_count):
-                print(data_format[i][j])
-                d = customtkinter.CTkLabel(master=self.scrollable_frame,
-                                           corner_radius=10,
-                                           text_color='black',
-                                           fg_color='grey',
-                                           text=data_format[i][j])
-                if j == 0:
-                    d.grid(row=(i + 4), column=j, columnspan=2, sticky="ew")
-                    continue
-                if j == 1:
-                    d.grid(row=(i + 4), column=(j+1), sticky="ew")
-                    continue
-                if j == 2:
-                    d.grid(row=(i+4), column=(j+1), columnspan=3, sticky="ew")
-                    continue
-
-
-
     def config(self):
         # configure the attribute
+        # tab 1 attributes
         self.btn_filechooser.configure(command=self.filechooser_event)
         self.btn_generate_example.configure(command=self.display_example_event)
         self.btn_clear_sample.configure(command=self.clear_string_sample)
+        self.btn_send_message.configure(command=self.button_trigger_event)
         self.option_delay.set('1')
-        self.tabview.set(self.tab1)
+        self.tabview.set(self.TAB1)
         self.string_filepath.set("./w.xlsx")
         self.scaling_option_menu.set("100%")
 
@@ -225,6 +235,9 @@ class WhatsappParentNoticeGUI(customtkinter.CTk):
         self.geometry(f"{900}x{380}")
         self.change_appearance_mode_event("深色模式")  # set default appearance
 
+        # backend whatsapp message sending initialisation
+        self.whatsapp = WhatsappParentNotice()
+
         # configure grid layout
         self.grid_columnconfigure(1, weight=2)
         self.grid_columnconfigure((0, 1, 2, 3), weight=2)
@@ -244,53 +257,42 @@ class WhatsappParentNoticeGUI(customtkinter.CTk):
         self.tabview = customtkinter.CTkTabview(self, anchor="nw")
         self.tabview.grid(row=0, column=1, rowspan=7, columnspan=4, padx=10, pady=10, sticky="ew")
 
-        self.tab1 = "回家/留宿通知"
-        self.tab2 = "使用须知"
-        self.tabview.add(self.tab1)
-        self.tabview.add(self.tab2)
+        self.TAB1 = "监护人通知"
+        self.TAB3 = "使用须知"
+        self.tabview.add(self.TAB1)
+        self.tabview.add(self.TAB3)
 
-        # tab 1 attributes
-        # section 1: file chooser
+        # tab 1 attributes initialization
         self.string_filepath = customtkinter.StringVar()
-        self.entry_filepath = customtkinter.CTkEntry(self.tabview.tab(self.tab1), textvariable=self.string_filepath,
-                                                     state="disable")
-        self.btn_filechooser = customtkinter.CTkButton(master=self.tabview.tab(self.tab1), text="打开文件",
+        self.btn_filechooser = customtkinter.CTkButton(master=self.tabview.tab(self.TAB1), text="打开文件",
                                                        fg_color="transparent", border_width=2)
-
-        # section 2: add a button to generate sample
-        self.btn_clear_sample = customtkinter.CTkButton(self.tabview.tab(self.tab1), text="清除")
-        self.btn_generate_example = customtkinter.CTkButton(self.tabview.tab(self.tab1), text="生成范本")
-
-        # section 3: display leaving sample
+        self.btn_clear_sample = customtkinter.CTkButton(self.tabview.tab(self.TAB1), text="清除")
+        self.btn_generate_example = customtkinter.CTkButton(self.tabview.tab(self.TAB1), text="生成范本")
         self.string_leaving_sample = customtkinter.StringVar()
-        self.entry_leaving_sample = customtkinter.CTkEntry(master=self.tabview.tab(self.tab1),
-                                                           textvariable=self.string_leaving_sample,
-                                                           state="disable")
-
-        # section 4: display staying message
         self.string_staying_sample = customtkinter.StringVar()
-        self.entry_staying_sample = customtkinter.CTkEntry(master=self.tabview.tab(self.tab1),
-                                                           textvariable=self.string_staying_sample,
-                                                           state="disable")
-
-        # section 5: attribute adjustment
-        self.option_delay = customtkinter.CTkOptionMenu(master=self.tabview.tab(self.tab1),
+        self.option_delay = customtkinter.CTkOptionMenu(master=self.tabview.tab(self.TAB1),
                                                         dynamic_resizing=False,
                                                         values=['0.5', '1', '1.5', '2', '2.5',
                                                                 '3', '3.5', '4', '4.5', '5'])
 
-        # section 6: button to send the whatsapp message
-        self.btn_send_message = customtkinter.CTkButton(master=self.tabview.tab(self.tab1), text='批量发送信息')
+        self.STRING_BATCH_SEND = "批量发送信息"
+        self.STRING_CANCEL_SEND = "中断发送"
+        self.btn_send_message = customtkinter.CTkButton(master=self.tabview.tab(self.TAB1),
+                                                        text=self.STRING_BATCH_SEND)
+        # end of tab 1
 
-        # tab 2 attribute
-        self.scrollable_frame = customtkinter.CTkScrollableFrame(master=self.tabview.tab(self.tab2), width=600)
+        # tab 3 attribute
+        self.scrollable_frame = customtkinter.CTkScrollableFrame(master=self.tabview.tab(self.TAB3), width=600)
 
         self.tab1_grid_init()
-        self.tab2_grid_init()
+        # self.tab3_grid_init()
 
         self.config()
 
 
 if __name__ == "__main__":
-    app = WhatsappParentNoticeGUI()
-    app.mainloop()
+    try:
+        app = WhatsappParentNoticeGUI()
+        app.mainloop()
+    except Exception as error:
+        messagebox.showerror(message=str(error))
--- WhatsappSendMessage.py
+++ WhatsappSendMessage.py
@@ -3,29 +3,40 @@ import pyautogui
 import time
 import pandas as pd
 import pyperclip
+from tkinter import messagebox
+
+from pytesseract import pytesseract
 
 
 class WhatsappParentNotice:
+    # (留舍、离舍)批量通知表格须有属性
+    staying_leaving_columns = ['寝室', '姓名', '日期', '时间', '监护人电话', '离舍', '其他信息']
 
     def send_whatsapp_message(self, phone_number, message):
         # whatsapp_url = f"whatsapp://send?phone={phone_number}&text={message}"
-        whatsapp_url = f"whatsapp://send?phone={phone_number}&text=\"\""
+        whatsapp_url = f"whatsapp://send?phone={phone_number}"
         webbrowser.open(whatsapp_url)
 
         # Wait for WhatsApp to open
         time.sleep(self.delay)  # Adjust the wait time as needed
         pyperclip.copy(message)
         pyautogui.hotkey('ctrl', 'v')
-        time.sleep(self.delay)
         pyautogui.press('enter')
 
         # Wait for the message to be sent
         time.sleep(self.delay)  # Adjust the wait time as needed
 
     def run(self):
+        self.set_stop(False)
         df = pd.read_excel(self.source_file, index_col='姓名')
 
         for name, rows in df.iterrows():
+
+            if not pd.isna(rows['其他信息']):
+                message = rows['其他信息']
+                self.send_whatsapp_message(rows['监护人电话'], message)
+                continue
+
             if rows['离舍'] == 0:
                 message = f"{name} 同学 {rows['寝室']} 于 {str(rows['日期'])[:10]} 本周留舍 (留校)。\n敬请家长/监护人关注。"
                 self.send_whatsapp_message(rows['监护人电话'], message)
@@ -36,6 +47,9 @@ class WhatsappParentNotice:
                 message = f'{name} 同学 {rows['寝室']} 于 {timing} 离校 (回家)。\n敬请家长/监护人关注。'
                 self.send_whatsapp_message(rows['监护人电话'], message)
 
+            if self.stop_sending:
+                messagebox.showinfo(message=f'已停止发送。\n最后一位信息接收者：{name}')
+                break
 
     @staticmethod
     def get_date_of_week(date):
@@ -51,11 +65,22 @@ class WhatsappParentNotice:
         day_name = date.strftime("%A")
         return switcher.get(day_name, '')
 
-    def __init__(self, txt_source_file, float_delay=1):
+    def __init__(self):
+        self.source_file = ""
+        self.delay = 1
+        self.stop_sending = False
+
+    def set_value(self, txt_source_file, float_delay=1):
         self.source_file = txt_source_file
         self.delay = float_delay
 
+    def set_stop(self, status: bool):
+        self.stop_sending = status
+
 
 if __name__ == '__main__':
-    app = WhatsappParentNotice('w.xlsx')
+    app = WhatsappParentNotice()
+    app.set_value("w.xlsx")
+    print('called in main function')
+    time.sleep(1)
     app.run()
